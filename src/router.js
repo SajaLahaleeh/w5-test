236e2ed3b11d6999d50bf803c3f2dbd6f57fbf4d
yusralahleeh <yusra.lahaleeh@gmail.com>
2019-07-21 12:40:35 +0300

    I solve it

--- src/router.js
+++ src/router.js
@@ -1,17 +1,16 @@
-const {handleHomeRoute, handlePublic, handleAPI} = require('./handlers.js');
+const { handleHomeRoute, handlePublic, handleAPI } = require("./handlers.js");
 
 const router = (request, response) => {
   const url = request.url;
-  if (url === '/') {
+  if (url === "/") {
     handleHomeRoute(request, response);
-  } else if (url.indexOf('public') !== -1) {
+  } else if (url.indexOf("public") !== -1) {
     handlePublic(request, response, url);
-  } else if (url === '/api') {
-    handleAPI(request, response)
-  }
-  else {
-    response.writeHead(404, {'Content-Type': 'text/html'});
-    response.end('<h1>404 file not found</h1>');
+  } else if (url.indexOf("/api") !== -1) {
+    handleAPI(request, response, url);
+  } else {
+    response.writeHead(404, { "Content-Type": "text/html" });
+    response.end("<h1>404 file not found</h1>");
   }
 };
 
